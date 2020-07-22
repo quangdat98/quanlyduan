d37a65d2092687c8e4e28a27348398716a099363
datnq <dat26011998@gmail.com>
2020-07-22 15:30:54 +0700

    Initial commit

---
+++ src/main/java/com/example/quanlyduan/QuanlyduanApplication.java
@@ -0,0 +1,13 @@
+package com.example.quanlyduan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlyduanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanlyduanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quanlyduan/QuanlyduanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quanlyduan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlyduanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
