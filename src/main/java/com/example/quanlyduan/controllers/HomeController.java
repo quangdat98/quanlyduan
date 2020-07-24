c32bf3926e9f657a4a147294689f6bf12c0d6306
datnq <dat26011998@gmail.com>
2020-07-24 18:33:14 +0700

    datnq add phần hiện

--- src/main/java/com/example/quanlyduan/controllers/HomeController.java
+++ src/main/java/com/example/quanlyduan/controllers/HomeController.java
@@ -2,10 +2,12 @@ package com.example.quanlyduan.controllers;
 
 import com.example.quanlyduan.data.model.Employee;
 import com.example.quanlyduan.data.service.EmployeeService;
+import com.example.quanlyduan.data.service.SolutionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -13,15 +15,24 @@ import java.util.List;
 public class HomeController {
 
     private EmployeeService employeeService;
+    private SolutionService solutionService;
+
     @Autowired
     public void setEmployeeService(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
 
-    @ResponseBody
+    @Autowired
+    public void setSolutionService(SolutionService solutionService) {
+        this.solutionService = solutionService;
+    }
+
     @RequestMapping("/")
-    public List<Employee> home(){
-        return employeeService.findAll();
+    public ModelAndView home(){
+        ModelAndView model = new ModelAndView();
+        model.addObject("listSolution",solutionService.findAllSoution());
+        model.setViewName("home");
+        return model;
     }
 }
 
